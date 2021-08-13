b48b33cae7a97b71a0cd5b97aa271a5bdc2cd4f5
Olzhas31 <suleimenov97@gmail.com>
2021-08-13 14:19:23 +0600

    Initial commit

---
+++ src/main/java/kaz/suleimenov/spring_course_zt/springboot/springboot_rest/SpringbootRestApplication.java
@@ -0,0 +1,13 @@
+package kaz.suleimenov.spring_course_zt.springboot.springboot_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kaz/suleimenov/spring_course_zt/springboot/springboot_rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package kaz.suleimenov.spring_course_zt.springboot.springboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
